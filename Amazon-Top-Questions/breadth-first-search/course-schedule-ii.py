71af37cfe9eb7578819e960974f81fbd9a592d7a
Ashish Gaur <ashishgaurp91@gmail.com>
2020-10-25 10:25:22 +0530

    Indegree solution to course schedule II

--- Amazon-Top-Questions/breadth-first-search/course-schedule-ii.py
+++ Amazon-Top-Questions/breadth-first-search/course-schedule-ii.py
@@ -1,3 +1,4 @@
+## Using topological sorting
 class Solution:
   def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
     graph = defaultdict(list)
@@ -29,3 +30,31 @@ class Solution:
         dfs(v)
 
     return topological_order if not has_loop else []
+
+## Using indegree computation
+from collections import defaultdict, deque
+class Solution:
+  def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    adj_list = defaultdict(list)
+    indeg = {}
+
+    for dest, src in prerequisites:
+      adj_list[src].append(dest)
+      indeg[dest] = indeg.get(dest, 0) + 1
+
+    print(adj_list)
+    zero_indeg = deque([v for v in range(numCourses) if v not in indeg])
+    print(zero_indeg)
+
+    topological_order = []
+    while len(zero_indeg) > 0:
+      v = zero_indeg.popleft()
+      topological_order.append(v)
+
+      if v in adj_list:
+        for neighbour in adj_list[v]:
+          indeg[neighbour] -= 1
+          if indeg[neighbour] == 0:
+            zero_indeg.append(neighbour)
+
+    return topological_order if len(topological_order) == numCourses else []
